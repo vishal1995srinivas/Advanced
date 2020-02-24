e7e2a253e8f33e328035cffd1c63be1c7f6a6e3f
Vishal Srinivas <vishal1995srinivas@gmail.com>
2020-02-24 17:55:09 +0530

    Update Item Mutation

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -11,6 +11,22 @@ const Mutations = {
 		);
 
 		return item;
+	},
+	updateItem(parent, args, ctx, info) {
+		//first take a copy of updates
+		const updates = { ...args };
+		//remove ID from the updates
+		delete updates.id;
+		//run the update method
+		return ctx.db.mutation.updateItem(
+			{
+				data: updates,
+				where: {
+					id: args.id
+				}
+			},
+			info
+		);
 	}
 };
 
