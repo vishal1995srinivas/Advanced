975574aac9697f56f69c7458ee4281ef8397e872
Vishal Srinivas <vishal1995srinivas@gmail.com>
2020-02-24 17:26:53 +0530

    Uploading Image

--- frontend/components/CreateItem.js
+++ frontend/components/CreateItem.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
+import Router from 'next/router';
 import Form from './styles/Form';
 import formatMoney from '../lib/formatMoney';
 import Error from './ErrorMessage';
@@ -18,68 +19,102 @@ const CREATE_ITEM_MUTATION = gql`
 		}
 	}
 `;
+//ToDo: Disable submit button (OR (Show progress bar)) until image and largeimage url arrive from cloudinary. User clicking submit immediately after uploading images might be a problem here.
 class CreateItem extends Component {
-	constructor(props) {
-		super(props);
-
-		this.state = {
-			title: 'Shoes',
-			description: 'I love this description',
-			image: 'dog.png',
-			largeImage: 'large_dog.png',
-			price: 1000
-		};
-	}
+	state = {
+		title: 'Cool Shoes',
+		description: 'I love those shoes',
+		image: 'dog.jpg',
+		largeImage: 'large-dog.jpg',
+		price: 1000
+	};
 	handleChange = (e) => {
 		const { name, type, value } = e.target;
 		const val = type === 'number' ? parseFloat(value) : value;
 		this.setState({ [name]: val });
 	};
-
+	uploadFile = async (e) => {
+		console.log('Uploading File');
+		const files = e.target.files;
+		const data = new FormData();
+		data.append('file', files[0]);
+		data.append('upload_preset', 'sickfits');
+		const res = await fetch('https://api.cloudinary.com/v1_1/dugfprmwz/image/upload', {
+			method: 'POST',
+			body: data
+		});
+		const file = await res.json();
+		console.log(file);
+		this.setState({
+			image: file.secure_url,
+			largeImage: file.eager[0].secure_url
+		});
+	};
 	render() {
 		return (
 			<Mutation mutation={CREATE_ITEM_MUTATION} variables={this.state}>
 				{(createItem, { loading, error }) => (
 					<Form
-						onSubmit={(e) => {
+						onSubmit={async (e) => {
+							// Stop the form from submitting
 							e.preventDefault();
-							console.log(this.state);
+							// call the mutation
+							const res = await createItem();
+							// change them to the single item page
+							console.log(res);
+							//Similar to link tag
+							Router.push({
+								pathname: '/item',
+								query: { id: res.data.createItem.id }
+							});
 						}}
 					>
 						<Error error={error} />
 						<fieldset disabled={loading} aria-busy={loading}>
+							<label htmlFor="file">
+								Image
+								<input
+									type="file"
+									id="file"
+									name="file"
+									placeholder="Upload an Image"
+									required
+									onChange={this.uploadFile}
+								/>
+							</label>
 							<label htmlFor="title">
 								Title
 								<input
 									type="text"
 									id="title"
 									name="title"
-									required
 									placeholder="Title"
+									required
 									value={this.state.title}
 									onChange={this.handleChange}
 								/>
 							</label>
+
 							<label htmlFor="price">
 								Price
 								<input
 									type="number"
 									id="price"
 									name="price"
-									required
 									placeholder="Price"
+									required
 									value={this.state.price}
 									onChange={this.handleChange}
 								/>
 							</label>
-							<label htmlFor="price">
+
+							<label htmlFor="description">
 								Description
-								<input
-									type="text"
+								<textarea
 									id="description"
 									name="description"
+									placeholder="Enter A Description"
 									required
-									placeholder="Enter a Description"
 									value={this.state.description}
 									onChange={this.handleChange}
 								/>
@@ -87,9 +122,11 @@ class CreateItem extends Component {
 							<button type="submit">Submit</button>
 						</fieldset>
 					</Form>
-				)}>
+				)}
 			</Mutation>
 		);
 	}
 }
+
 export default CreateItem;
+export { CREATE_ITEM_MUTATION };
