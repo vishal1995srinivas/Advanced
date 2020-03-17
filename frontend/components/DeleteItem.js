a56faa086c01b1164ce419b63fbc740f89833ce1
Vishal Srinivas <vishal1995srinivas@gmail.com>
2020-03-17 13:13:23 +0530

    Delete feature- filtering cache

--- frontend/components/DeleteItem.js
+++ frontend/components/DeleteItem.js
@@ -23,7 +23,7 @@ class DeleteItem extends Component {
 		console.log(data, payload);
 		//1. Read the cache for the items we want
 		//2. Filter the deleted item out of the page
-		// data.items = data.items.filter((item) => item.id !== payload.data.deleteItem.id);
+		data.items = data.items.filter((item) => item.id !== payload.data.deleteItem.id);
 		//Put the items back
 		// cache.writeQuery({ query: ALL_ITEMS_QUERY, data: data });
 	};
