b3c929e8b986dfd21001049de8e09f9947ce9aed
Vishal Srinivas <vishal1995srinivas@gmail.com>
2020-03-24 13:24:13 +0530

    Skip and first added with perPAge fix

--- frontend/components/Pagination.js
+++ frontend/components/Pagination.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PaginationStyles from './styles/PaginationStyles';
 import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
-import perPage from '../config';
+import { perPage } from '../config';
 import Head from 'next/head';
 import Link from 'next/link';
 const PAGINATION_QUERY = gql`
@@ -14,13 +14,12 @@ const PAGINATION_QUERY = gql`
 		}
 	}
 `;
-
 const Pagination = (props) => (
 	<Query query={PAGINATION_QUERY}>
 		{({ data, loading, error }) => {
 			if (loading) return <p>Loading...</p>;
 			const count = data.itemsConnection.aggregate.count;
-			const pages = Math.ceil(count / 4); //perPage is not loading from config
+			const pages = Math.ceil(count / perPage);
 			return (
 				<PaginationStyles>
 					<Head>
