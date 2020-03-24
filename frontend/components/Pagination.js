7ce5ccee3c8c364fd97b163c68112846a674877e
Vishal Srinivas <vishal1995srinivas@gmail.com>
2020-03-24 12:11:50 +0530

    pagination with next and prev buttons added

--- frontend/components/Pagination.js
+++ frontend/components/Pagination.js
@@ -3,6 +3,8 @@ import PaginationStyles from './styles/PaginationStyles';
 import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
 import perPage from '../config';
+import Head from 'next/head';
+import Link from 'next/link';
 const PAGINATION_QUERY = gql`
 	query PAGINATION_QUERY {
 		itemsConnection {
@@ -14,20 +16,48 @@ const PAGINATION_QUERY = gql`
 `;
 
 const Pagination = (props) => (
-	<PaginationStyles>
-		<Query query={PAGINATION_QUERY}>
-			{({ data, loading, error }) => {
-				if (loading) return <p>Loading...</p>;
-				const count = data.itemsConnection.aggregate.count;
-				const pages = Math.ceil(count / 4); //perPage is not loading from config
-				return (
+	<Query query={PAGINATION_QUERY}>
+		{({ data, loading, error }) => {
+			if (loading) return <p>Loading...</p>;
+			const count = data.itemsConnection.aggregate.count;
+			const pages = Math.ceil(count / 4); //perPage is not loading from config
+			return (
+				<PaginationStyles>
+					<Head>
+						<title>
+							Sick Fits -- {props.page} of {pages}
+						</title>
+					</Head>
+					<Link
+						prefetch
+						href={{
+							pathname: 'Items',
+							query: { page: props.page - 1 }
+						}}
+					>
+						<a className="prev" aria-disabled={props.page <= 1}>
+							⬅ Prev
+						</a>
+					</Link>
 					<p>
 						Page {props.page} of {pages}
 					</p>
-				);
-			}}
-		</Query>
-	</PaginationStyles>
+					<p>{count} Items Total</p>
+					<Link
+						prefetch
+						href={{
+							pathname: 'Items',
+							query: { page: props.page + 1 }
+						}}
+					>
+						<a className="prev" aria-disabled={props.page >= pages}>
+							Next ➡
+						</a>
+					</Link>
+				</PaginationStyles>
+			);
+		}}
+	</Query>
 );
 
 export default Pagination;
