593dbac37946570de3b39a7d22d6e3518fe920b8
Vishal Srinivas <vishal1995srinivas@gmail.com>
2020-03-24 13:25:14 +0530

    perpage

--- frontend/components/Items.js
+++ frontend/components/Items.js
@@ -31,7 +31,10 @@ class Items extends Component {
 		return (
 			<Center>
 				<Pagination page={this.props.page} />
-				<Query query={ALL_ITEMS_QUERY} variables={{ skip: this.props.page * perPage - perPage, first: 4 }}>
+				<Query
+					query={ALL_ITEMS_QUERY}
+					variables={{ skip: this.props.page * perPage - perPage, first: perPage }}
+				>
 					{({ data, error, loading }) => {
 						if (loading) return <p>Loading...</p>;
 						if (error) return <p>Error: {error.message}</p>;
