06089b8aaf29a0770c8050c0170d11e87b1908aa
Vishal Srinivas <vishal1995srinivas@gmail.com>
2020-04-06 15:24:49 +0530

    please sign in!

--- frontend/components/PleaseSignIn.js
+++ frontend/components/PleaseSignIn.js
@@ -4,12 +4,13 @@ import Signin from './Signin';
 
 const PleaseSignIn = (props) => (
 	<Query query={CURRENT_USER_QUERY}>
-		{(data, loading) => {
+		{({ data, loading }) => {
 			if (loading) return <p>Loading...</p>;
 			if (!data.me) {
 				return (
 					<div>
 						<p>Please sign in before continuing</p>
+						<Signin />
 					</div>
 				);
 			}
