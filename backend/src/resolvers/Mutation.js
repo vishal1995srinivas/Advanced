0ae9e18ba12b47bac81c581d51acb11bab8e997c
Vishal Srinivas <vishal1995srinivas@gmail.com>
2020-04-06 13:30:54 +0530

    added user field to item in db and added for createItem

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -6,11 +6,19 @@ const { transport, makeANiceEmail } = require('../mail');
 
 const Mutations = {
 	async createItem(parent, args, ctx, info) {
-		// TODO: Check if they are logged in
+		if (!ctx.request.userId) {
+			throw new Error('You must be logged in to perform this operation');
+		}
 
 		const item = await ctx.db.mutation.createItem(
 			{
 				data: {
+					//Providing data-relationship in prisma
+					user: {
+						connect: {
+							id: ctx.request.userId
+						}
+					},
 					...args
 				}
 			},
