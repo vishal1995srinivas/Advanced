5cda593911c51fe9b4cff16ada278ef1428d0c35
Vishal Srinivas <vishal1995srinivas@gmail.com>
2020-04-10 11:38:49 +0530

    Find the cart item

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -248,89 +248,20 @@ const Mutations = {
 			},
 			info
 		);
-	}
-	// async removeFromCart(parent, args, ctx, info) {
-	//   // 1. Find the cart item
-	//   const cartItem = await ctx.db.query.cartItem(
-	//     {
-	//       where: {
-	//         id: args.id,
-	//       },
-	//     },
-	//     `{ id, user { id }}`
-	//   );
-	//   // 1.5 Make sure we found an item
-	//   if (!cartItem) throw new Error('No CartItem Found!');
-	//   // 2. Make sure they own that cart item
-	//   if (cartItem.user.id !== ctx.request.userId) {
-	//     throw new Error('Cheatin huhhhh');
-	//   }
-	//   // 3. Delete that cart item
-	//   return ctx.db.mutation.deleteCartItem(
-	//     {
-	//       where: { id: args.id },
-	//     },
-	//     info
-	//   );
-	// },
-	// async createOrder(parent, args, ctx, info) {
-	//   // 1. Query the current user and make sure they are signed in
-	//   const { userId } = ctx.request;
-	//   if (!userId) throw new Error('You must be signed in to complete this order.');
-	//   const user = await ctx.db.query.user(
-	//     { where: { id: userId } },
-	//     `{
-	//     id
-	//     name
-	//     email
-	//     cart {
-	//       id
-	//       quantity
-	//       item { title price id description image largeImage }
-	//     }}`
-	//   );
-	//   // 2. recalculate the total for the price
-	//   const amount = user.cart.reduce(
-	//     (tally, cartItem) => tally + cartItem.item.price * cartItem.quantity,
-	//     0
-	//   );
-	//   console.log(`Going to charge for a total of ${amount}`);
-	//   // 3. Create the stripe charge (turn token into $$$)
-	//   const charge = await stripe.charges.create({
-	//     amount,
-	//     currency: 'USD',
-	//     source: args.token,
-	//   });
-	//   // 4. Convert the CartItems to OrderItems
-	//   const orderItems = user.cart.map(cartItem => {
-	//     const orderItem = {
-	//       ...cartItem.item,
-	//       quantity: cartItem.quantity,
-	//       user: { connect: { id: userId } },
-	//     };
-	//     delete orderItem.id;
-	//     return orderItem;
-	//   });
+	},
+	async removeFromCart(parent, args, ctx, info) {
+		//TODO
+		//Find the cart item
+		const cartIitem = await ctx.db.query.cartItem({
+			where: {
+				id: args.id
+			}
+		});
 
-	//   // 5. create the Order
-	//   const order = await ctx.db.mutation.createOrder({
-	//     data: {
-	//       total: charge.amount,
-	//       charge: charge.id,
-	//       items: { create: orderItems },
-	//       user: { connect: { id: userId } },
-	//     },
-	//   });
-	//   // 6. Clean up - clear the users cart, delete cartItems
-	//   const cartItemIds = user.cart.map(cartItem => cartItem.id);
-	//   await ctx.db.mutation.deleteManyCartItems({
-	//     where: {
-	//       id_in: cartItemIds,
-	//     },
-	//   });
-	//   // 7. Return the Order to the client
-	//   return order;
-	// },
+		//Check the item is present in the cart.
+		//Make sure the item is owned by the user.
+		//delete the cart item.
+	}
 };
 
 module.exports = Mutations;
