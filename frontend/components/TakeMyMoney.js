f12b65baa6fd488924c6c0fe82e537a6c6ee5f03
Vishal Srinivas <vishal1995srinivas@gmail.com>
2020-04-11 21:26:35 +0530

    stripe giving us token

--- frontend/components/TakeMyMoney.js
+++ frontend/components/TakeMyMoney.js
@@ -13,16 +13,21 @@ function TotalItems(cart) {
 }
 
 class TakeMyMoney extends Component {
+	onToken = (res) => {
+		console.log(res);
+	};
 	render() {
 		return (
 			<User>
 				{({ data: { me } }) => (
-					<StripeCheckout
+					<StripeCheckout //Provide as much info possible so that chances of declining is low.
+						currency="USD"
 						stripeKey="pk_test_556JXNBdRYtNmeVoBl4VsXCH00jHsLusuc"
 						amount={calcTotalPrice(me.cart)}
 						name="Sick-fits Online Store"
 						image={me.cart[0].item && me.cart[0].item.image}
 						description={`Order of ${TotalItems(me.cart)}`}
+						token={(res) => this.onToken(res)}
 					>
 						{this.props.children}
 					</StripeCheckout>
