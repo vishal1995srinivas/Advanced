d41516e1780b47ed3bcf63bc9abb50ec5dc625c2
Vishal Srinivas <vishal1995srinivas@gmail.com>
2020-04-11 11:16:05 +0530

    Search items query error fix

--- frontend/components/Search.js
+++ frontend/components/Search.js
@@ -8,7 +8,11 @@ import { DropDown, DropDownItem, SearchStyles } from './styles/DropDown';
 
 const SEARCH_ITEMS_QUERY = gql`
 	query SEARCH_ITEMS_QUERY($searchTerm: String!) {
-		items(where: { OR: [{ title_contains: $searchTerm }, { descriptions_contains: $searchTerm }] })
+		items(where: { OR: [{ title_contains: $searchTerm }, { description_contains: $searchTerm }] }) {
+			id
+			image
+			title
+		}
 	}
 `;
 
