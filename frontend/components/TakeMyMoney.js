b98ac40f5d160b256bb52562b3df0ba6994db352
Vishal Srinivas <vishal1995srinivas@gmail.com>
2020-04-11 23:11:43 +0530

    create order token supplied

--- frontend/components/TakeMyMoney.js
+++ frontend/components/TakeMyMoney.js
@@ -7,6 +7,7 @@ import gql from 'graphql-tag';
 import calcTotalPrice from '../lib/calcTotalPrice';
 import Error from './ErrorMessage';
 import User, { CURRENT_USER_QUERY } from './User';
+import { Mutation } from 'react-apollo';
 
 const CURRENT_ORDER_MUTATION = gql`
 	mutation createOrder($token: String!) {
@@ -26,24 +27,36 @@ function TotalItems(cart) {
 }
 
 class TakeMyMoney extends Component {
-	onToken = (res) => {
-		console.log(res);
+	onToken = (res, createOrder) => {
+		console.log(res.id);
+		//Manually call the mutation once we have the stripe token.
+		createOrder({
+			variables: {
+				token: res.id
+			}
+		}).catch((err) => {
+			alert(err.message);
+		});
 	};
 	render() {
 		return (
 			<User>
 				{({ data: { me } }) => (
-					<StripeCheckout //Provide as much info possible so that chances of declining is low.
-						currency="USD"
-						stripeKey="pk_test_556JXNBdRYtNmeVoBl4VsXCH00jHsLusuc"
-						amount={calcTotalPrice(me.cart)}
-						name="Sick-fits Online Store"
-						image={me.cart[0].item && me.cart[0].item.image}
-						description={`Order of ${TotalItems(me.cart)}`}
-						token={(res) => this.onToken(res)}
-					>
-						{this.props.children}
-					</StripeCheckout>
+					<Mutation mutation={CURRENT_ORDER_MUTATION} refetchQueries={[ { query: CURRENT_USER_QUERY } ]}>
+						{(createOrder) => (
+							<StripeCheckout //Provide as much info possible so that chances of declining is low.
+								currency="USD"
+								stripeKey="pk_test_556JXNBdRYtNmeVoBl4VsXCH00jHsLusuc"
+								amount={calcTotalPrice(me.cart)}
+								name="Sick-fits Online Store"
+								image={me.cart[0].item && me.cart[0].item.image}
+								description={`Order of ${TotalItems(me.cart)}`}
+								token={(res) => this.onToken(res, createOrder)}
+							>
+								{this.props.children}
+							</StripeCheckout>
+						)}
+					</Mutation>
 				)}
 			</User>
 		);
