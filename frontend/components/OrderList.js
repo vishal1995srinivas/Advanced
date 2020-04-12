80236e483dba662919d4561918348349c231cb3d
Vishal Srinivas <vishal1995srinivas@gmail.com>
2020-04-12 13:54:53 +0530

    Order list

--- frontend/components/OrderList.js
+++ frontend/components/OrderList.js
@@ -7,9 +7,37 @@ import gql from 'graphql-tag';
 import formatMoney from '../lib/formatMoney';
 import OrderItemStyles from './styles/OrderItemStyles';
 
+const USER_ORDERS_QUERY = gql`
+	query USER_ORDERS_QUERY {
+		orders(orderBy: createdAt_DESC) {
+			orders(orderBy: createdAt_DESC) {
+				id
+				total
+				createdAt
+				items {
+					id
+					title
+					price
+					description
+					quantity
+					image
+				}
+			}
+		}
+	}
+`;
+
 class OrderList extends Component {
 	render() {
-		return <div />;
+		return (
+			<Query query={USER_ORDERS_QUERY}>
+				{({ data: { orders }, loading, error }) => {
+					if (loading) return <p>Loading...</p>;
+					if (error) return <Error error={error} />;
+					<p>You have {orders.length} orders</p>;
+				}}
+			</Query>
+		);
 	}
 }
 
