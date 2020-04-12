6d1cc458d6ece3b1210c2fd023b3c9e8545385a1
Vishal Srinivas <vishal1995srinivas@gmail.com>
2020-04-12 16:52:49 +0530

    single order completed finally

--- frontend/components/Order.js
+++ frontend/components/Order.js
@@ -22,6 +22,9 @@ const SINGLE_ORDER_QUERY = gql`
 				id
 				title
 				description
+				image
+				quantity
+				price
 			}
 		}
 	}
@@ -34,26 +37,49 @@ class Order extends Component {
 	render() {
 		return (
 			<Query query={SINGLE_ORDER_QUERY} variables={{ id: this.props.id }}>
-				{({ data, error, loading }) => {
+				{({ data: { order }, error, loading }) => {
 					if (loading) return <p>Loading...</p>;
 					if (error) return <Error error={error} />;
+					//console.log(order);
 					return (
 						<OrderStyles>
 							<Head>
-								<title> Sick-Fits -Order {Order.id}</title>
+								<title> Sick-Fits -Order {order.id}</title>
 							</Head>
 							<p>
-								<span>Order Id: </span>
+								<span>Order Id : </span>
 								<span>{this.props.id}</span>
 							</p>
 							<p>
-								<span>Charge</span>
-								<span>{Order.charge}</span>
+								<span>Charge : </span>
+								<span>{order.charge}</span>
 							</p>
 							<p>
-								<span>Date</span>
+								<span>Date : </span>
 								<span>{format(order.createdAt, 'MMMM d, YYYY h:mm a')}</span>
 							</p>
+							<p>
+								<span>Order Total : </span>
+								<span>{formatMoney(order.total)} only</span>
+							</p>
+							<p>
+								<span>Item Count : </span>
+								<span>{order.items.length}</span>
+							</p>
+							<div className="items">
+								{order.items.map((item) => (
+									<div className="order-item" key={item.id}>
+										<img src={item.image} alt={item.title} />
+										<div className="item-details">
+											<h2>{item.title}</h2>
+											<p>Qty: {item.quantity}</p>
+											<p>Each : {formatMoney(item.price)}</p>
+											<p>SubTotal: {formatMoney(item.price * item.quantity)}</p>
+											<p>{item.description}</p>
+										</div>
+									</div>
+								))}
+							</div>
 						</OrderStyles>
 					);
 				}}
