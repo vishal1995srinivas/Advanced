806d89decba5b8bf88518cb0239dfae4af8c92dd
Vishal Srinivas <vishal1995srinivas@gmail.com>
2020-04-15 22:39:23 +0530

    buttons in item component testd

--- frontend/__tests__/Item.test.js
+++ frontend/__tests__/Item.test.js
@@ -11,11 +11,26 @@ const fakeItem = {
 };
 
 describe('<Item/>', () => {
+	it('renders out the img ', () => {
+		const wrapper = shallow(<ItemComponent item={fakeItem} />);
+		const img = wrapper.find('img');
+		expect(img.props().src).toBe(fakeItem.image);
+		expect(img.props().alt).toBe(fakeItem.title);
+	});
 	it('renders and displays properly', () => {
 		const wrapper = shallow(<ItemComponent item={fakeItem} />);
 		const PriceTag = wrapper.find('PriceTag');
 		console.log(PriceTag.children());
 		expect(PriceTag.children().text()).toBe('$50');
 		expect(wrapper.find('Title a').text()).toBe(fakeItem.title);
+	});
+	it('renders out the buttons properly', () => {
+		const wrapper = shallow(<ItemComponent item={fakeItem} />);
+
+		//console.log(wrapper.debug());
+		const buttonList = wrapper.find('.buttonList');
+		expect(buttonList.children()).toHaveLength(3);
+		expect(buttonList.find('Link')).toHaveLength(1);
+		expect(buttonList.find('Link')).toBeTruthy(); //These both yield same results.
 	});
 });
