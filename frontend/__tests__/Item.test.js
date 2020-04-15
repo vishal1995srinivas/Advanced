054656c9ace2c369cb267817b1c8e2bcb535ad50
Vishal Srinivas <vishal1995srinivas@gmail.com>
2020-04-15 16:33:11 +0530

    shallow rendr test of whole item component

---
+++ frontend/__tests__/Item.test.js
@@ -0,0 +1,18 @@
+import ItemComponent from '../components/Item';
+import { shallow } from 'enzyme';
+
+const fakeItem = {
+	id: 'ABC123',
+	title: 'A Cool item',
+	price: 5000,
+	description: 'This item is very cool!',
+	image: 'dog.jpg',
+	largeImage: 'largedog.jpg'
+};
+
+describe('<Item/>', () => {
+	it('renders and displays properly', () => {
+		const wrapper = shallow(<ItemComponent item={fakeItem} />);
+		console.log(wrapper.debug());
+	});
+});
