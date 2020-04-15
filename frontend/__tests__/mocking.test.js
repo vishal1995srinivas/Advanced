8146471f0a9a76f08387fedfe310cba8b118f5a4
Vishal Srinivas <vishal1995srinivas@gmail.com>
2020-04-15 15:26:26 +0530

    mocking an api-simulated

--- frontend/__tests__/mocking.test.js
+++ frontend/__tests__/mocking.test.js
@@ -1,3 +1,15 @@
+function Person(name, foods) {
+	this.name = name;
+	this.foods = foods;
+}
+
+Person.prototype.fetchFavFoods = function() {
+	return new Promise((resolve, reject) => {
+		//Simulating an api
+		setTimeout(() => resolve(this.foods), 2000);
+	});
+};
+
 describe('mocking learning', () => {
 	it('mocks a reg function', () => {
 		const fetchDogs = jest.fn();
@@ -6,5 +18,11 @@ describe('mocking learning', () => {
 		expect(fetchDogs).toHaveBeenCalledWith('snickers');
 		fetchDogs('hugo');
 		expect(fetchDogs).toHaveBeenCalledTimes(2);
+	});
+
+	it('can create a person', async () => {
+		const me = new Person('Vis', [ 'pizza', 'burgers' ]);
+		const favFoods = await me.fetchFavFoods();
+		expect(favFoods).toContain('pizza');
 	});
 });
