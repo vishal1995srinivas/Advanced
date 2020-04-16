221d97b331a6d3e60671faaa76bcf41418632030
Vishal Srinivas <vishal1995srinivas@gmail.com>
2020-04-16 23:34:50 +0530

    Please SignIn tests completed

--- frontend/__tests__/PleaseSignIn.test.js
+++ frontend/__tests__/PleaseSignIn.test.js
@@ -1,10 +1,9 @@
 import { mount } from 'enzyme';
-import toJSON from 'enzyme-to-json';
 import wait from 'waait';
-import { fakeUser } from '../lib/testUtils';
-import { MockedProvider } from 'react-apollo/test-utils';
-import { CURRENT_USER_QUERY } from '../components/SingleItem';
 import PleaseSignIn from '../components/PleaseSignIn';
+import { CURRENT_USER_QUERY } from '../components/User';
+import { MockedProvider } from 'react-apollo/test-utils';
+import { fakeUser } from '../lib/testUtils';
 
 const notSignedInMocks = [
 	{
@@ -33,7 +32,8 @@ describe('<PleaseSignIn/>', () => {
 		const SignIn = wrapper.find('Signin');
 		expect(SignIn.exists()).toBe(true);
 	});
-	it('renders out the child component when user is logged in', async () => {
+
+	it('renders the child component when the user is signed in', async () => {
 		const Hey = () => <p>Hey!</p>;
 		const wrapper = mount(
 			<MockedProvider mocks={signedInMocks}>
@@ -45,6 +45,8 @@ describe('<PleaseSignIn/>', () => {
 
 		await wait();
 		wrapper.update();
-		console.log(wrapper.debug());
+		//console.log(wrapper.debug());
+		// expect(wrapper.find('Hey').exists()).toBe(true);
+		expect(wrapper.contains(<Hey />)).toBe(true);
 	});
 });
