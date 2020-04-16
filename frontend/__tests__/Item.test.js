249bb899aaf1ce84c5c5fcb64fd3cc55d43d31b5
Vishal Srinivas <vishal1995srinivas@gmail.com>
2020-04-16 13:51:07 +0530

    Snapshot testing

--- frontend/__tests__/Item.test.js
+++ frontend/__tests__/Item.test.js
@@ -9,28 +9,37 @@ const fakeItem = {
 	image: 'dog.jpg',
 	largeImage: 'largedog.jpg'
 };
+//Unit Testing
+// describe('<Item/>', () => {
+// 	it('renders out the img ', () => {
+// 		const wrapper = shallow(<ItemComponent item={fakeItem} />);
+// 		const img = wrapper.find('img');
+// 		expect(img.props().src).toBe(fakeItem.image);
+// 		expect(img.props().alt).toBe(fakeItem.title);
+// 	});
+// 	it('renders and displays properly', () => {
+// 		const wrapper = shallow(<ItemComponent item={fakeItem} />);
+// 		const PriceTag = wrapper.find('PriceTag');
+// 		console.log(PriceTag.children());
+// 		expect(PriceTag.children().text()).toBe('$50');
+// 		expect(wrapper.find('Title a').text()).toBe(fakeItem.title);
+// 	});
+// 	it('renders out the buttons properly', () => {
+// 		const wrapper = shallow(<ItemComponent item={fakeItem} />);
 
+// 		//console.log(wrapper.debug());
+// 		const buttonList = wrapper.find('.buttonList');
+// 		expect(buttonList.children()).toHaveLength(3);
+// 		expect(buttonList.find('Link')).toHaveLength(1);
+// 		expect(buttonList.find('Link')).toBeTruthy(); //These both yield same results.
+// 		expect(buttonList.find('AddToCart')).toBeTruthy();
+// 		expect(buttonList.find('DeleteItem')).toBeTruthy();
+// 	});
+// });
+// Snapshot Testing
 describe('<Item/>', () => {
-	it('renders out the img ', () => {
+	it('renders and matched the component', () => {
 		const wrapper = shallow(<ItemComponent item={fakeItem} />);
-		const img = wrapper.find('img');
-		expect(img.props().src).toBe(fakeItem.image);
-		expect(img.props().alt).toBe(fakeItem.title);
-	});
-	it('renders and displays properly', () => {
-		const wrapper = shallow(<ItemComponent item={fakeItem} />);
-		const PriceTag = wrapper.find('PriceTag');
-		console.log(PriceTag.children());
-		expect(PriceTag.children().text()).toBe('$50');
-		expect(wrapper.find('Title a').text()).toBe(fakeItem.title);
-	});
-	it('renders out the buttons properly', () => {
-		const wrapper = shallow(<ItemComponent item={fakeItem} />);
-
-		//console.log(wrapper.debug());
-		const buttonList = wrapper.find('.buttonList');
-		expect(buttonList.children()).toHaveLength(3);
-		expect(buttonList.find('Link')).toHaveLength(1);
-		expect(buttonList.find('Link')).toBeTruthy(); //These both yield same results.
+		expect(wrapper).toMatchSnapshot();
 	});
 });
